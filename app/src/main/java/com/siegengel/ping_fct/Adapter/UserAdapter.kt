d3190ea61c0a6f0fd8d7ea0e70a682bd2780afbb
angel <a22angelgp@iessanclemente.net>
2024-06-11 16:56:19 +0200

    (*) Bug fixed && Last Message Correct && New Rebranding

--- app/src/main/java/com/siegengel/ping_fct/Adapter/UserAdapter.kt
+++ app/src/main/java/com/siegengel/ping_fct/Adapter/UserAdapter.kt
@@ -8,18 +8,58 @@ import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 import com.siegengel.ping_fct.MessageActivity
+import com.siegengel.ping_fct.Model.Chat
 import com.siegengel.ping_fct.Model.User
 import com.siegengel.ping_fct.R
 
+
 class UserAdapter(private val mContext: Context, private val mUsers: List<User>, private val isChat: Boolean) :
     RecyclerView.Adapter<UserAdapter.ViewHolder>() {
+
+        private lateinit var lastmessage: String
 
     class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         var username: TextView = itemView.findViewById(R.id.contact_name)
         var profilePicture: ImageView = itemView.findViewById(R.id.contact_picture)
         var imageStatusOn: ImageView = itemView.findViewById(R.id.contact_status_on)
         var imageStatusOff: ImageView = itemView.findViewById(R.id.contact_status_off)
+        var lastMessage: TextView = itemView.findViewById(R.id.last_send)
+    }
+
+    private fun lastMessage(userid: String, last_msg: TextView) {
+        lastmessage = "default"
+        val firebaseUser = FirebaseAuth.getInstance().currentUser
+        val reference = FirebaseDatabase.getInstance().getReference("Chats")
+
+        reference.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                for (snapshot in dataSnapshot.children) {
+                    val chat = snapshot.getValue(Chat::class.java)
+                    if (firebaseUser != null && chat != null) {
+                        if (chat.getReceiver() == firebaseUser.uid && chat.getSender() == userid ||
+                            chat.getReceiver() == userid && chat.getSender() == firebaseUser.uid
+                        ) {
+                            lastmessage = chat.getMessage()!!
+                        }
+                    }
+                }
+
+                when (lastmessage) {
+                    "default" -> last_msg.text = ""
+                    else -> last_msg.text = lastmessage
+                }
+                lastmessage = "default"
+            }
+
+            override fun onCancelled(databaseError: DatabaseError) {
+            }
+        })
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
@@ -38,6 +78,12 @@ class UserAdapter(private val mContext: Context, private val mUsers: List<User>,
             holder.profilePicture.setImageResource(R.drawable.default_profile_picture)
         } else {
             Glide.with(mContext).load(user.getImageURL()).into(holder.profilePicture)
+        }
+
+        if (isChat) {
+            lastMessage(user.getId(), holder.lastMessage)
+        } else {
+            holder.lastMessage.visibility = View.INVISIBLE
         }
 
         if (isChat) {
